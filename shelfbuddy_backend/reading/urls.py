ad8cde8114e1142f10ee2263e20a858632c58bef
Britaney Winston <b.winston87@yahoo.com>
2025-04-30 21:29:29 -0500

    Update to plans logic.

--- shelfbuddy_backend/reading/urls.py
+++ shelfbuddy_backend/reading/urls.py
@@ -1,4 +1,4 @@
-from .views import create_reading_plan, reading_plan_detail, log_progress, weekly_report, check_falling_behind,reading_dashboard, create_goal_view, delete_goal,about_shelfbuddy, edit_reading_plan, reading_plan_detail
+from .views import create_reading_plan, reading_plan_detail, log_progress, weekly_report, check_falling_behind,reading_dashboard, create_goal_view, delete_goal,about_shelfbuddy, edit_reading_plan, reading_plan_detail, apply_suggested_target
 from django.urls import path
 
 
@@ -14,4 +14,5 @@ path('goal/<int:goal_id>/delete/', delete_goal, name='delete_goal'),
 path('about/', about_shelfbuddy, name='about_shelfbuddy'),
 path('reading-plan/<int:plan_id>/edit/',edit_reading_plan, name='edit_reading_plan'),
 path('plan/<int:plan_id>/',reading_plan_detail, name='reading_plan_detail'),
+path('plan/<int:plan_id>/apply/', apply_suggested_target, name='apply_suggested_target'),
 ]
